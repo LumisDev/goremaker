9191bdefb3dadc240c6a060021a80e464a0fe50f
LumisDev <153009693+LumisDev@users.noreply.github.com>
2025-01-12 10:18:42 +0100

    Create basic windowing

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
+const glfw = @import("zglfw");
+const zgpu = @import("zgpu");
+const wgpu = zgpu.wgpu;
 
 pub fn main() !void {
-    
+    try glfw.init();
+    const window = try glfw.createWindow(640, 480, "Hello!", null);
+    defer glfw.destroyWindow(window);
+    while (!glfw.windowShouldClose(window)) {
+        glfw.pollEvents();
+    }
 }
