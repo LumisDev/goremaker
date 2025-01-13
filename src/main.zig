3f5ab72d694f490b2dcaa5114d952bd1868950e0
LumisDev <153009693+LumisDev@users.noreply.github.com>
2025-01-13 16:46:38 +0100

    Added shader comp

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const glfw = @import("zglfw");
 const zgpu = @import("zgpu");
 const wgpu = zgpu.wgpu;
-const shader = @embedFile("shader.wgsl");
+const shader_source = @embedFile("shader.wgsl");
 pub fn main() !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(general_purpose_allocator.deinit() == .ok);
@@ -12,23 +12,25 @@ pub fn main() !void {
     const window = try glfw.Window.create(640, 480, "Hello!", null);
     defer window.destroy();
     const gctx = try zgpu.GraphicsContext.create(
-    allocator,
-    .{
-        .window = window,
-        .fn_getTime = @ptrCast(&glfw.getTime),
-        .fn_getFramebufferSize = @ptrCast(&glfw.Window.getFramebufferSize),
+        allocator,
+        .{
+            .window = window,
+            .fn_getTime = @ptrCast(&glfw.getTime),
+            .fn_getFramebufferSize = @ptrCast(&glfw.Window.getFramebufferSize),
 
-        // optional fields
-        .fn_getWin32Window = @ptrCast(&glfw.getWin32Window),
-        .fn_getX11Display = @ptrCast(&glfw.getX11Display),
-        .fn_getX11Window = @ptrCast(&glfw.getX11Window),
-        .fn_getWaylandDisplay = @ptrCast(&glfw.getWaylandDisplay),
-        .fn_getWaylandSurface = @ptrCast(&glfw.getWaylandWindow),
-        .fn_getCocoaWindow = @ptrCast(&glfw.getCocoaWindow),
-    },
-    .{}, // default context creation options
+            // optional fields
+            .fn_getWin32Window = @ptrCast(&glfw.getWin32Window),
+            .fn_getX11Display = @ptrCast(&glfw.getX11Display),
+            .fn_getX11Window = @ptrCast(&glfw.getX11Window),
+            .fn_getWaylandDisplay = @ptrCast(&glfw.getWaylandDisplay),
+            .fn_getWaylandSurface = @ptrCast(&glfw.getWaylandWindow),
+            .fn_getCocoaWindow = @ptrCast(&glfw.getCocoaWindow),
+        },
+        .{}, // default context creation options
     );
     defer gctx.destroy(allocator);
+    const shader_module = zgpu.createWgslShaderModule(gctx.device, shader_source, "shader module");
+    defer shader_module.release();
     while (!glfw.windowShouldClose(window)) {
         glfw.pollEvents();
     }
