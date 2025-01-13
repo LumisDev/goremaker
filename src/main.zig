13a99b65103fa3448659f55605e3366743690452
LumisDev <153009693+LumisDev@users.noreply.github.com>
2025-01-13 16:31:57 +0100

    Added new changes

--- src/main.zig
+++ src/main.zig
@@ -9,9 +9,26 @@ pub fn main() !void {
     const allocator = general_purpose_allocator.allocator();
     try glfw.init();
     defer glfw.terminate();
-    const window = try glfw.createWindow(640, 480, "Hello!", null);
-    defer glfw.destroyWindow(window);
-    const ctx = zgpu.GraphicsContext.create(allocator)
+    const window = try glfw.Window.create(640, 480, "Hello!", null);
+    defer window.destroy();
+    const gctx = try zgpu.GraphicsContext.create(
+    allocator,
+    .{
+        .window = window,
+        .fn_getTime = @ptrCast(&glfw.getTime),
+        .fn_getFramebufferSize = @ptrCast(&glfw.Window.getFramebufferSize),
+
+        // optional fields
+        .fn_getWin32Window = @ptrCast(&glfw.getWin32Window),
+        .fn_getX11Display = @ptrCast(&glfw.getX11Display),
+        .fn_getX11Window = @ptrCast(&glfw.getX11Window),
+        .fn_getWaylandDisplay = @ptrCast(&glfw.getWaylandDisplay),
+        .fn_getWaylandSurface = @ptrCast(&glfw.getWaylandWindow),
+        .fn_getCocoaWindow = @ptrCast(&glfw.getCocoaWindow),
+    },
+    .{}, // default context creation options
+    );
+    defer gctx.destroy(allocator);
     while (!glfw.windowShouldClose(window)) {
         glfw.pollEvents();
     }
